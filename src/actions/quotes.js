9073989e697163cd977750973efbede053873aa7
ReginaF2012 <skritschy@gmail.com>
2020-08-25 14:56:08 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,19 @@
-// TODO: Create action creators as defined in tests
+
+export const addQuote = (quote) => {
+    //? Is this because we want the key of quote equal to the quote object?
+    //? is it the same as doing 'quote: quote'
+    return {type: 'ADD_QUOTE', quote}
+}
+
+export const removeQuote = (quoteId) => {
+    //? would we/should we do this as return {type: "REMOVE_QUOTE", payload: quoteId}
+    return {type: 'REMOVE_QUOTE', quoteId}
+}
+
+export const upvoteQuote = (quoteId) => {
+    return {type: "UPVOTE_QUOTE", quoteId}
+}
+
+export const downvoteQuote = (quoteId) => {
+    return {type: "DOWNVOTE_QUOTE", quoteId}
+}
