b57f76f1a47b1397cab5c12eba78dd74c4a80a6d
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-28 16:38:46 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes.js';
 
 class Quotes extends Component {
 
   render() {
+    const {quotes, upvoteQuote, downvoteQuote, removeQuote} = this.props
     return (
       <div>
         <hr />
@@ -15,11 +17,18 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
+            {console.log("this", this.props)}
+               {/* {QuoteCard(this.props.dispatch)} */}
+              {/* const { name, realName } = hero
                 TODO:
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+               {quotes.map(quote => <QuoteCard key={quote.id} 
+               upvoteQuote={upvoteQuote}
+               downvoteQuote={downvoteQuote}
+               removeQuote={removeQuote}
+              quote={quote} />)}
             </div>
           </div>
         </div>
@@ -28,5 +37,20 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+return {
+  quotes: state.quotes
+}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeQuote: (quoteId) => dispatch(removeQuote(quoteId)),
+    downvoteQuote: (quoteId) => dispatch(downvoteQuote(quoteId)),
+    upvoteQuote: (quoteId) => dispatch(upvoteQuote(quoteId))
+  }
+}
+
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,7 @@ import { combineReducers } from 'redux';
 import quotes from './quotes';
 
 export default combineReducers({
+
   quotes
 });
+
