dbc9883e33e48b8582f030e676a64639aa277e61
Bryce-Du <bryce.dumas@gmail.com>
2020-08-30 19:15:18 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,13 @@
 // TODO: Create action creators as defined in tests
+export function addQuote(quote){
+    return {type: "ADD_QUOTE", quote: quote}
+}
+export function removeQuote(quoteID){
+    return {type: "REMOVE_QUOTE", quoteId: quoteID}
+}
+export function upvoteQuote(quoteID){
+    return {type: "UPVOTE_QUOTE", quoteId: quoteID}
+}
+export function downvoteQuote(quoteID){
+    return {type: "DOWNVOTE_QUOTE", quoteId: quoteID}
+}
--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -4,6 +4,11 @@ import QuoteCard from '../components/QuoteCard';
 
 class Quotes extends Component {
 
+  renderQuotes = () => {
+    console.log(this.props.quotes)
+    this.props.quotes.map((quote, id) => <QuoteCard quote={quote} key={id}/>)
+  }
+  
   render() {
     return (
       <div>
@@ -15,11 +20,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {this.renderQuotes()}
             </div>
           </div>
         </div>
@@ -29,4 +30,4 @@ class Quotes extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(state => ({ quotes: state.quotes }))(Quotes);
